95aed5025c78490eaadc06c9dc69c338d611840b
romuloalves <me@romuloalv.es>
2019-05-17 15:46:38 -0300

    Fixes code
    Former-commit-id: 37492b56c24e74902e2595552a133ee0e09d12c7
    Former-commit-id: 413da1d84ce7c7b01775eb2dceaa7e9e4799cfd3

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,8 +4,8 @@ import { List } from 'react-native-paper';
 export default function ListItem({ title, description, onClick, left, right }) {
   return (
     <List.Item
-      title={title}
-      description={description}
+      title={ title }
+      description={ description }
       onPress={ () => onClick() }
       left={ left }
       right={ right }>
